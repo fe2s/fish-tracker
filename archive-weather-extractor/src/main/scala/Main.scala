ed96af92b2579151843f8a6ccebe98776bb760f0
fe2s <aka.fe2s@gmail.com>
2014-01-07 22:14:22 +0200

    integrational test

--- archive-weather-extractor/src/main/scala/Main.scala
+++ archive-weather-extractor/src/main/scala/Main.scala
@@ -1,6 +1,18 @@
 import scala.concurrent._
+import com.github.nscala_time.time.Imports._
+
 
 object Main extends App {
 
-//    future {}
+  val today = new DateTime()
+  val earlier = today.minusDays(2)
+
+  val ws = WeatherSource(earlier)
+  val loader = new PageLoader()
+  val html = loader.load(ws.url)
+  val extractor = new Extractor()
+  val records = extractor.extract(html)
+  println(s"records = $records")
+
+
 }
